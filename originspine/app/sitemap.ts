13ec68a22f1da3853d4dd8f3951eb764cc501086
Shivam Kumar <codedpilott@gmail.com>
2026-06-08 21:35:27 +0530

    Update sitemap.ts

--- originspine/app/sitemap.ts
+++ originspine/app/sitemap.ts
@@ -1,32 +1,48 @@
-export default function sitemap() {
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
       url: "https://origin-spine.vercel.app",
       lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
     },
     {
       url: "https://origin-spine.vercel.app/physiotherapy-in-patna",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
     },
     {
       url: "https://origin-spine.vercel.app/slip-disc-treatment-patna",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.9,
     },
     {
       url: "https://origin-spine.vercel.app/back-pain-treatment-patna",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
     {
       url: "https://origin-spine.vercel.app/knee-pain-treatment-patna",
       lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
     },
     {
       url: "https://origin-spine.vercel.app/how-we-treat",
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
     },
     {
       url: "https://origin-spine.vercel.app/problems",
       lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
     },
   ];
 }
