b41a0cf2023b017d5c6fb314770d661611139748
Shivam Kumar <codedpilott@gmail.com>
2026-06-08 20:53:33 +0530

    Create sitemap.ts

---
+++ originspine/sitemap.ts
@@ -0,0 +1,32 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://origin-spine.vercel.app",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://origin-spine.vercel.app/physiotherapy-in-patna",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://origin-spine.vercel.app/slip-disc-treatment-patna",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://origin-spine.vercel.app/back-pain-treatment-patna",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://origin-spine.vercel.app/knee-pain-treatment-patna",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://origin-spine.vercel.app/how-we-treat",
+      lastModified: new Date(),
+    },
+    {
+      url: "https://origin-spine.vercel.app/problems",
+      lastModified: new Date(),
+    },
+  ];
+}
