0a9e08e65b0bc7bd3fc11ca4f6f27f7d07432346
Shivam Kumar <codedpilott@gmail.com>
2026-06-08 22:36:10 +0530

    Update next.config.ts

--- originspine/next.config.ts
+++ originspine/next.config.ts
@@ -1,7 +1,17 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/xml",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
